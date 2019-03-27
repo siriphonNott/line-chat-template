94ae3c2a5a83a45309fa706c1b7777b0577ca33f
siriphonnot <siriphonnot@gmail.com>
2019-03-28 04:30:31 +0700

    Add update profile for only follow

--- index.js
+++ index.js
@@ -194,47 +194,48 @@ app.post('/webhook', async function (req, res) {
         updates[`users/${userId}/updatedAt`] = events.timestamp;
         database.ref().update(updates);
         console.log('==> [User update successfully]');
-        
-        //Get Profile
-        client.getProfile(userId)
-        .then(function (profile) {
-          console.log('[Get Profile]: successfully!');
-          console.log(profile);
-          if(profile) {
-            database.ref(`users/${userId}/profile`).set(profile, function (error) {
-              if (error) {
-                console.log('==> [Add profile fail]: ' + error);
-              } else {
-                console.log('==> [Add profile successfully]');
-              }
-            });
-            let greetingMessage = [
-              {
-                type: "text",
-                text: `Hello  ${profile.displayName} \n\nWelcome to Botty Chat Bot :)\n\nStatus: ${profile.statusMessage || '-'}`
-             },
-              {
-                type: "image",
-                originalContentUrl: profile.pictureUrl,
-                previewImageUrl: profile.pictureUrl,
-              }
-          ]
-            pushMessage(events.source.userId, greetingMessage)
-              .then((res) => {
-                console.log('==> [Push Greeting message successfully]');
-              })
-              .catch((e) => {
-                console.log('==> [Error]: ');
-                console.log(e);
-              })
-
-          }
-        })
-        .catch(function (error) {
-          console.log('[Get Profile]: Error');
-          console.log(error);
-          reject(error);
-        });
+        if(events.type === 'follow') {
+          //Get Profile
+          client.getProfile(userId)
+          .then(function (profile) {
+            console.log('[Get Profile]: successfully!');
+            console.log(profile);
+            if(profile) {
+              database.ref(`users/${userId}/profile`).set(profile, function (error) {
+                if (error) {
+                  console.log('==> [Add profile fail]: ' + error);
+                } else {
+                  console.log('==> [Add profile successfully]');
+                }
+              });
+              let greetingMessage = [
+                {
+                  type: "text",
+                  text: `Hello  ${profile.displayName} \n\nWelcome to Botty Chat Bot :)\n\nStatus: ${profile.statusMessage || '-'}`
+               },
+                {
+                  type: "image",
+                  originalContentUrl: profile.pictureUrl,
+                  previewImageUrl: profile.pictureUrl,
+                }
+            ]
+              pushMessage(events.source.userId, greetingMessage)
+                .then((res) => {
+                  console.log('==> [Push Greeting message successfully]');
+                })
+                .catch((e) => {
+                  console.log('==> [Error]: ');
+                  console.log(e);
+                })
+  
+            }
+          })
+          .catch(function (error) {
+            console.log('[Get Profile]: Error');
+            console.log(error);
+            reject(error);
+          });
+        }
       }
     });
   }
