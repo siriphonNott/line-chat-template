111c12a5158071797c3dc1822aee9070d6fd0779
siriphonnot <siriphonnot@gmail.com>
2019-06-04 23:13:11 +0700

    Change Line Config

--- config/lineDevelopConfig.js
+++ config/lineDevelopConfig.js
@@ -1,6 +1,7 @@
 const config = {
-    appName: 'Line Chat Bot',
-    channelAccessToken: 'kbHe+0XH1G5aXIRxzUZHr0UggxswKqbeOT4xDIsloLnYyNWfsnwVWx02rmt2NUMY+ISwL9Z4mn+XhAYQTDypvTpOXWsLzM94ZoZaXoD0JrrohTGzehzrDVfqc5kQPxtOXrX3VlOrsaUhTsgkKgv1CAdB04t89/1O/w1cDnyilFU=',
-    channelSecret: '87a0e19ae700ea68d5d5380aac08187c',
+    appName: 'BOT Cute',
+    channelAccessToken: 'rs8LoIPXlY7SIRTQ9Duhg2gHxjFvIIVx8HEVkYl1sCrqSlg1k1kAIFvbxrSUKh0LgJiZZvdQHmpv6SGkaCuvDYj+8iMXQbRYZQ2kso42oiX3VLEHi9UQMolLbkYxUjQ7F2nl51jPasqNvRvuRLdjkgdB04t89/1O/w1cDnyilFU=',
+    channelSecret: 'b44e5277a0070a3e4c85f821d23d8780',
+    channelID: '1578647149'
 }
 exports.config = config;
